51af657c777a36f333e3de2bafad268813f3ab92
cauarojas <cauarojas@hotmail.com>
2024-04-15 08:37:59 -0300

    added darkmode support

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { isDarkMode, setDarkMode } from './utils/dark-mode'
+
 function App() {
     return (
-        <>
-            <h1 class="text-4xl">Hello World!</h1>
-        </>
+        <main
+            class="dark:bg-black h-full w-full transition-all duration-500"
+            // prettier-ignore
+            classList={{ "dark": isDarkMode() }}
+        >
+            <h1 class="dark:text-white text-4xl">Hello World!</h1>
+            <button
+                class="dark:text-white"
+                onclick={() => {
+                    setDarkMode(!isDarkMode())
+                }}
+            >
+                AAAAA
+            </button>
+        </main>
     )
 }
 
